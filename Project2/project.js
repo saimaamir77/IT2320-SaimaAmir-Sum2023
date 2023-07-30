2db7c9125348c02e33c86de14033703a74755ce2
saimaamir77 <saafpk@gmail.com>
2023-07-30 18:31:37 -0400

    Project 2

---
+++ Project2/project.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+function salesByPerson() {
+   alert("Display data in this alert box.");
+}
+
+function bestAndWorst() {
+   alert("Display data in this alert box.");
+}
+
+function commissionsByMonth() {
+   alert("Display data in this alert box.");
+}
